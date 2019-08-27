b9560233648c3617ec1d8db57d82944e885d8bf0
Unknown <miguekos1233@gmail.com>
2019-08-27 02:49:44 -0500

    upload first

---
+++ src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <TablaListado />
+  </q-page>
+</template>
+
+<style></style>
+
+<script>
+export default {
+  name: "PageIndex",
+  mounted() {
+    console.log("Todo esta ok");
+  },
+  components: {
+    TablaListado: () => import("./cotiza/TablaListado")
+  }
+};
+</script>
