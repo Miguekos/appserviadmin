8cadf7fc6870196c4986d124c3c529076bf7ed6e
Unknown <miguekos1233@gmail.com>
2019-09-10 02:07:29 -0500

    updates

---
+++ src/pages/chartjs/Chart.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "Chart.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/chartjs/ChartContainer.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ChartContainer.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
